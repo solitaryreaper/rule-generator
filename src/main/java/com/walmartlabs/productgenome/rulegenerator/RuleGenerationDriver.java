b98e49b600ab6ebc1c4e53a0bd38cf218bbab7e1
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2014-01-27 08:44:02 -0800

    Initial project structure commit for the auto entity matching rule generation project.

---
+++ src/main/java/com/walmartlabs/productgenome/rulegenerator/RuleGenerationDriver.java
@@ -0,0 +1,21 @@
+package com.walmartlabs.productgenome.rulegenerator;
+
+/**
+ * The main driver class for the auto-rule generation engine. It has the following functions :
+ * 
+ * 1) Load the cleaned data.
+ * 2) Generate feature vectors using attribute simmetric recommender library and string matching
+ *    core library.
+ * 3) Generate matching rules using a learning algorithm.
+ * 4) Evaluate the accuracy of each of the generated rules.
+ * 5) Output summary as well as detailed output of the performance of rules.
+ * @author excelsior
+ *
+ */
+public class RuleGenerationDriver {
+
+	public static void main(String[] args) {
+
+	}
+
+}
